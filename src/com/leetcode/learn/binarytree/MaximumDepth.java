a2f596f799b03191e445dcee70adee63af729977
chavezdev <jctecenge@gmail.com>
2025-01-10 11:30:36 -0300

    feat: maximum depth first commit

---
+++ src/com/leetcode/learn/binarytree/MaximumDepth.java
@@ -0,0 +1,9 @@
+package com.leetcode.learn.binarytree;
+/**
+ * leetcode: https://leetcode.com/problems/maximum-depth-of-binary-tree?envType=study-plan-v2&envId=top-100-liked
+ * */
+public class MaximumDepth {
+    public int maxDepth(TreeNode root) {
+        return 0;
+    }
+}
---
+++ src/com/leetcode/test/MaximumDepthTest.java
@@ -0,0 +1,11 @@
+package com.leetcode.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class MaximumDepthTest {
+
+    @Test
+    void maxDepth() {
+    }
+}
